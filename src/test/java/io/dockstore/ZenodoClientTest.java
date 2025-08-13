f832be6ac197f0e79f9c34087c20af391c0c50b7
Denis Yuen <denis.yuen@oicr.on.ca>
2025-08-13 15:25:09 -0400

    Add first working build, no tests

--- src/test/java/io/dockstore/ZenodoClientTest.java
+++ src/test/java/io/dockstore/ZenodoClientTest.java
@@ -1,29 +1,30 @@
 package io.dockstore;
 
+import io.openapi.invenio.ApiClient;
+import io.openapi.invenio.ApiException;
+import io.openapi.invenio.api.RecordsApi;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class ZenodoClientTest {
 
-//    private ApiClient client;
-//    private PreviewApi previewApi;
-//
-//    @BeforeEach
-//    void setup() {
-//        client = new ApiClient();
-//        client.setBasePath("https://sandbox.zenodo.org/api");
-//        previewApi = new io.swagger.zenodo.client.api.PreviewApi(client);
-//    }
-//
-//    @Test
-//    @Disabled("Test disabled as the communities API is failing on sandbox")
-//    public void testZenodoClient() throws ApiException {
-//        HashMap<String, Object> o = (HashMap<String, Object>)previewApi.listCommunities();
-//        assertTrue(o != null && !o.keySet().isEmpty(), "not able to list communities as basic test");
-//    }
-//
-//    @Test
-//    void testConceptDoi() {
-//        final SearchResult searchResult = previewApi.listRecords(null, "bestmatch", 1, 100);
-//        assertNotNull(searchResult.getHits().getHits().get(0).getConceptdoi());
-//        assertNotNull(searchResult.getHits().getHits().get(0).getCreated());
-//        assertNotNull(searchResult.getHits().getHits().get(0).getModified());
-//    }
+    private RecordsApi recordsApi;
+
+    @BeforeEach
+    void setup() {
+        ApiClient client = new ApiClient();
+        client.setBasePath("https://inveniordm.web.cern.ch/api");
+        recordsApi = new RecordsApi(client);
+    }
+
+    @Test
+    public void testRecordsSearch() throws ApiException {
+        // uh oh, their openapi is incomplete and has no objects
+        recordsApi.searchRecords(null, "bestmatch", 10, 0, true);
+    }
+
+    @Test
+    void testConceptDoi() {
+
+    }
 }
