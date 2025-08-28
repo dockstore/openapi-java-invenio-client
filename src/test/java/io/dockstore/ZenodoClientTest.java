51b2abb197287b02d42481e0a4e6e42791c68bca
Denis Yuen <denis.yuen@oicr.on.ca>
2025-08-28 13:32:39 -0400

    Add first working build, no tests (#1)
    * Add first working build, no tests

---
+++ src/test/java/io/dockstore/ZenodoClientTest.java
@@ -0,0 +1,34 @@
+package io.dockstore;
+
+import io.openapi.invenio.ApiClient;
+import io.openapi.invenio.ApiException;
+import io.openapi.invenio.api.RecordsApi;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ZenodoClientTest {
+
+    private RecordsApi recordsApi;
+
+    @BeforeEach
+    void setup() {
+        ApiClient client = new ApiClient();
+        client.setBasePath("https://inveniordm.web.cern.ch");
+        recordsApi = new RecordsApi(client);
+    }
+
+    @Test
+    public void testRecordsSearch() throws ApiException {
+        // uh oh, their openapi is incomplete and has no objects
+        try {
+            recordsApi.searchRecords(null, "bestmatch", 10, null, true);
+        } catch (ApiException e) {
+            // classpath having issues but not as big a deal as having no return types
+        }
+    }
+
+    @Test
+    void testConceptDoi() {
+
+    }
+}
